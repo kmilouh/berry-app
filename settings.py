6a81447171e8fc57b4f9a8c0d012d1433597ac32
agarcia-ruiz <90185326+agarcia-ruiz@users.noreply.github.com>
2022-12-15 00:03:18 +0000

    fix typo

--- settings.py
+++ settings.py
@@ -9,8 +9,8 @@ from decouple import config
 LOG_FOLDER= config('LOG_FOLDER', default="./logs/" ) 
 
 SLICER_EXEC = config('SLICER_EXEC', default="/programs/Slicer-4.8.1-linux-amd64/Slicer" ) 
-EMSEGMENTCOMMANDLINE = config('EMSEGMENTCOMMANDLINE', default=os.path.join(os.path.dirname(SLICER_EXEC,"/lib/Slicer-4.8/cli-modules/EMSegmentCommandLine")) )
-MRML= config('MRML', default=os.path.join(os.path.dirname(SLICER_EXEC,"/share/Slicer-4.8/qt-loadable-modules/EMSegment/Tasks/MRI-Human-Brain-Hemisphere.mrml")) )
+EMSEGMENTCOMMANDLINE = config('EMSEGMENTCOMMANDLINE', default=os.path.join(os.path.dirname(SLICER_EXEC),"/lib/Slicer-4.8/cli-modules/EMSegmentCommandLine") )
+MRML= config('MRML', default=os.path.join(os.path.dirname(SLICER_EXEC),"/share/Slicer-4.8/qt-loadable-modules/EMSegment/Tasks/MRI-Human-Brain-Hemisphere.mrml") )
 
 DCM2NIIX=config('DCM2NIIX', default="/programs/dcm2niix_lnx/dcm2niix" )
 
